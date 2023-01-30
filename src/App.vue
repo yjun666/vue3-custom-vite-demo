f6a445a15406e34f66c532bb4bf01f9d449a85ad
yjun666 <yjun_163_email@163.com>
2023-01-30 15:59:38 +0800

    测试vue3slot用法

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div>
-    <PageLog></PageLog>
-  </div>
+	<div>
+		<PageLog>
+			<template v-slot:slotName="data">
+				<div>
+					{{data.data}}
+				</div>
+			</template>
+		</PageLog>
+	</div>
 </template>
 
 <script setup lang="ts">
-import PageLog from "@/views/page-log/page-log.vue";
+import PageLog from '@/views/page-log/page-log.vue';
 </script>
 
 <style scoped lang="scss">
--- src/views/page-log/page-log.vue
+++ src/views/page-log/page-log.vue
@@ -1,6 +1,8 @@
 <template>
 	<!-- 页面打印及调试 -->
 	<div>
+		<slot :data="slotItem"
+			name="slotName"></slot>
 		<div>$utils.supplement()==== {{ $utils.supplement("") }}</div>
 		<div>$utils.uuid()==== {{ uuid() }}</div>
 		<div>VITE_CUSTOM_TEST==={{ VITE_CUSTOM_TEST(1, 2) }}</div>
@@ -11,7 +13,7 @@
 	</div>
 </template>
 <script lang="ts" setup>
-import { getCurrentInstance, computed } from 'vue';
+import { getCurrentInstance, computed, ref, inject } from 'vue';
 const instance: any = getCurrentInstance();
 const { uuid, formatDate } = instance.appContext.config.globalProperties.$utils;
 console.log('import.meta.env.VITE_CUSTOM_TEST===' + import.meta.env.VITE_CUSTOM_TEST);
@@ -27,6 +29,10 @@ let VITE_CUSTOM_TEST = computed(() => {
 let NODE_ENV = computed(() => {
 	return import.meta.env.MODE;
 });
+let slotItem = ref(0);
+setInterval(() => {
+	slotItem.value = slotItem.value + 1;
+}, 1000);
 </script>
 <style lang="scss" scoped>
 </style>
