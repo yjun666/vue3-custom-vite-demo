5527903a022044b815a2a4f5ecbff13ae34b9800
yjun666 <yjun_163_email@163.com>
2023-02-02 16:34:20 +0800

    配置element-plus 分页为中文， 配置element-plus样式f覆盖及scss变量

--- src/plugins/element-plus.ts
+++ src/plugins/element-plus.ts
@@ -1,10 +1,24 @@
+import { elThemeType, elStyleType } from '../config';
 // element-plus
 import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
+import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+// import 'element-plus/dist/index.css'; // 默认主题
+
+import(`@/style/element-ui/element-ui-base-${elStyleType}.scss`) // 引入element-ui样式表----------每次重置样式表都需要刷新页面才可以正常展示
+// require(`@/style/element-ui/element-ui-variable-${elThemeType}.scss`); // 导入修改过的主题文件
+import(`@/style/element-ui/element-ui-variable-${elThemeType}.scss`); // 导入修改过的主题文件
 
 export function setupElementPlus(app: any) {
+    zhCn.el.pagination = {
+        pagesize: '条/页',
+        total: '共{total}条',
+        goto: '前往',
+        pageClassifier: '页',
+        deprecationWarning: '',
+    }
     app.config.globalProperties.$ELEMENT = {
-    // element-plus 全局配置
+        // element-plus 全局配置
+        locale: zhCn
     };
     app.use(ElementPlus);
 }
