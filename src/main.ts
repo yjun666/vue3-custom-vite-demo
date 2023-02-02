a55ab6001743abfc63adeff8001945173b7d6c9a
yjun666 <yjun_163_email@163.com>
2023-02-02 19:41:04 +0800

    全局注入store

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import { setGlobal } from '@/plugins/v-global';
 // element-plus
 import { setupElementPlus } from '@/plugins/element-plus';
+import store from '@/store';
 // vue-i18n
 // 多语言
 import { setupI18n } from '@/plugins/vue-i18n';
@@ -12,9 +13,9 @@ import router from '@/router/index';
 
 import './style.css'
 // 在vite项目中需要使用import 不能使用require 使用import引入scss文件需要使用小括号
-import ('@/assets/fonts/iconfont.css');
-import (`@/assets/style/reset.scss`); // 导入重置全局样式文件
-import (`@/assets/style/common.scss`); // 导入全局样式文件
+import('@/assets/fonts/iconfont.css');
+import(`@/assets/style/reset.scss`); // 导入重置全局样式文件
+import(`@/assets/style/common.scss`); // 导入全局样式文件
 
 // 初始化app
 const app = createApp(App);
@@ -24,4 +25,4 @@ const app = createApp(App);
 setupElementPlus(app);
 setupI18n(app);
 setGlobal(app);
-app.use(router).mount('#app')
+app.use(store).use(router).mount('#app')
--- src/plugins/element-plus.ts
+++ src/plugins/element-plus.ts
@@ -1,12 +1,17 @@
-import { elThemeType, elStyleType } from '../config';
+import { elThemeType, elStyleType } from '@/config';
 // element-plus
 import ElementPlus from 'element-plus';
 import zhCn from 'element-plus/lib/locale/lang/zh-cn';
-// import 'element-plus/dist/index.css'; // 默认主题
 
-import(`@/style/element-ui/element-ui-base-${elStyleType}.scss`) // 引入element-ui样式表----------每次重置样式表都需要刷新页面才可以正常展示
+const a = elStyleType;
+const b = elThemeType;
+
+// import 'element-plus/dist/index.css'; // 默认主题
+// eslint-disable-next-line no-unused-expressions
+import(`@/style/element-ui/element-ui-base-${a}.scss`) // 引入element-ui样式表----------每次重置样式表都需要刷新页面才可以正常展示
 // require(`@/style/element-ui/element-ui-variable-${elThemeType}.scss`); // 导入修改过的主题文件
-import(`@/style/element-ui/element-ui-variable-${elThemeType}.scss`); // 导入修改过的主题文件
+// eslint-disable-next-line no-unused-expressions
+import(`@/style/element-ui/element-ui-variable-${b}.scss`); // 导入修改过的主题文件
 
 export function setupElementPlus(app: any) {
     zhCn.el.pagination = {
@@ -14,7 +19,7 @@ export function setupElementPlus(app: any) {
         total: '共{total}条',
         goto: '前往',
         pageClassifier: '页',
-        deprecationWarning: '',
+        deprecationWarning: ''
     }
     app.config.globalProperties.$ELEMENT = {
         // element-plus 全局配置
